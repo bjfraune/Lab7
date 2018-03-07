8d8cfa70b730cf1cfe24c44b24257a56b970a7bb
bjfraune <35780003+bjfraune@users.noreply.github.com>
2018-03-07 14:11:38 -0600

    added a getter for first name and last name

--- Employee.java
+++ Employee.java
@@ -1,42 +1,44 @@
-
 public class Employee {
 	private String _fname;
 	private String _lname;
 	private int _phone;
 	private String _department;
 
-	public Employee(String firstName, String lastName, int phoneNum, String department)
-	{
+	public Employee(String firstName, String lastName, int phoneNum,
+			String department) {
 		_fname = firstName;
 		_lname = lastName;
 		_phone = phoneNum;
 		_department = department;
 	}
-	
-	public void setFirstName(String name)
-	{
+
+	public void setFirstName(String name) {
 		_fname = name;
 	}
-	
-	public void setLastName(String name)
-	{
+
+	public void setLastName(String name) {
 		_lname = name;
 	}
+
+	public String getFirstName() {
+		return _fname;
+	}
 	
-	public void setPhoneNumber(int number)
-	{
+	public String getLastName() {
+		return _lname;
+	}
+
+	public void setPhoneNumber(int number) {
 		_phone = number;
 	}
-	
-	public void setDepartment(String department)
-	{
+
+	public void setDepartment(String department) {
 		_department = department;
 	}
-	
+
 	@Override
-	public String toString() 
-	{
+	public String toString() {
 		return _lname + ", " + _fname + " " + _phone + " " + _department + "\n";
 	}
-	
+
 }
