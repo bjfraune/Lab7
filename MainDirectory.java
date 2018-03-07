6b18085cb462e94eaa28a3e7c3eee5cba7295bb7
bjfraune <35780003+bjfraune@users.noreply.github.com>
2018-03-07 14:31:26 -0600

    stuff

--- MainDirectory.java
+++ MainDirectory.java
@@ -1,4 +1,48 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
 
-public class MainDirectory {
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+public class MainDirectory implements Comparator<Employee> {
+	ArrayList<Employee> directory;
+	Gson g;
+
+	public MainDirectory() {
+		directory = new ArrayList<Employee>();
+		g = new Gson();
+	}
+
+	public void add(ArrayList newEntries) {
+		directory.addAll(newEntries);
+//		directory.sort(c);
+	}
+
+	public void print() {
+		for(Employee person : directory){
+			System.out.println(person);
+		}
+	}
+
+	public void clear() {
+		directory.clear();
+	}
+
+	public void trasfer(String json) {
+		ArrayList<Employee> entries = (g.fromJson(json, new TypeToken<ArrayList<Employee>>(){}.getType()));
+		add(entries);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.util.Comparator#compare(java.lang.Object, java.lang.Object)
+	 * 
+	 * @return < 0 if o1 comes before o2 0 if o1 == o2 > 0 if o2 comes after o1
+	 */
+	@Override
+	public int compare(Employee o1, Employee o2) {
+		return o1.getLastName().compareTo(o2.getLastName());
+	}
 }
