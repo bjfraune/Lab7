04a8f328ba4aeb5bdc9dc04b2826c4153a593b33
FawziehA <35780330+FawziehA@users.noreply.github.com>
2018-03-07 14:26:51 -0600

    Update DirectoryEditor.java

--- DirectoryEditor.java
+++ DirectoryEditor.java
@@ -1,15 +1,12 @@
-import java.util.ArrayList;
-
 
 public class DirectoryEditor {
+
 	public static void main (String [] args){
+	
 		DirectoryProxy dp = new DirectoryProxy();
-		ArrayList <Employee> emp = new ArrayList<Employee>();
-		emp.add(new Employee("Adam","Smith",32444,"EE"));
-		emp.add(new Employee("Sarah","John",63254,"CF"));
-		emp.add(new Employee("Laura","John",63254,"CF"));
-		emp.add(new Employee("Henry","Jonathan",63254,"CF"));
-		
-		dp.add(emp);
+		dp.add(new Employee("Adam","Smith",32444,"EE"));
+		dp.add(new Employee("Sarah","John",63254,"CF"));
+		dp.add(new Employee("Laura","John",63254,"SF"));
+		dp.add(new Employee("Henry","Jonathan",63254,"NF"));
 	}
 }
