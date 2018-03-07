61bea1d0edddcb7fe5f097674268d928da34d18c
hanajc <35780386+hanajc@users.noreply.github.com>
2018-03-07 14:01:03 -0600

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -1,4 +1,42 @@
 
 public class Employee {
+	private String _fname;
+	private String _lname;
+	private int _phone;
+	private String _department;
 
+	public Employee(String firstName, String lastName, int phoneNum, String department)
+	{
+		_fname = firstName;
+		_lname = lastName;
+		_phone = phoneNum;
+		_department = department;
+	}
+	
+	public void setFirstName(String name)
+	{
+		_fname = name;
+	}
+	
+	public void setLastName(String name)
+	{
+		_lname = name;
+	}
+	
+	public void setPhoneNumber(int number)
+	{
+		_phone = number;
+	}
+	
+	public void setDepartment(String department)
+	{
+		_department = department;
+	}
+	
+	@Override
+	public String toString() 
+	{
+		return _lname + ", " + _fname + " " + _phone + " " + _department + "\n";
+	}
+	
 }
